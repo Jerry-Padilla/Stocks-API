a8077b7d526f5c09a5903f827d1e13ff77cd34c5
Gerardo R Padilla Jr <gerardopadillareynoso@gmail.com>
2023-06-26 16:48:00 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+stocksApi = "";
+
+async function FetchStonkInfo(stocksApi) {
+  const response = await fetch();
+  stonksData = await response.json();
+}
