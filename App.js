be9cf15c4b1e8bfe794f8d8a681ffb8cce9e1ffa
Gerardo R Padilla Jr <gerardopadillareynoso@gmail.com>
2023-06-28 17:31:31 -0700

    Update App.js

--- App.js
+++ App.js
@@ -4,45 +4,51 @@ async function FetchStonkInfo(symbol, min, sec) {
   tbl.style.display = "table";
   time = 1000 * (Number(sec) + 60 * Number(min));
   timeString = time.toString();
-  console.log(time);
+  //console.log(time);
   dispInfo(symbol, timeString);
 }
 
 async function dispInfo(symbol, time) {
+  //Call for new information every time.
   const response = await fetch(
     "https://finnhub.io/api/v1/quote?symbol=" +
       symbol +
       "&token=ci6cjr1r01qhmud1ipg0ci6cjr1r01qhmud1ipgg"
   );
+  //get the graph
   stonksData = await response.json();
-  console.log(stonksData);
+  var date = Date(stonksData.t);
+  //console.log(stonksData);
+
+  //get the table
   var tbodyRef = document
     .getElementById("myTable")
     .getElementsByTagName("tbody")[0];
 
+  //Add new row and the cells to the table
   var newRow = tbodyRef.insertRow();
-  var oCell = newRow.insertCell();
-  var hCell = newRow.insertCell();
-  var lCell = newRow.insertCell();
-  var cCell = newRow.insertCell();
-  var pCell = newRow.insertCell();
-  var tCell = newRow.insertCell();
+  var openCell = newRow.insertCell();
+  var highCell = newRow.insertCell();
+  var lowCell = newRow.insertCell();
+  var closingCell = newRow.insertCell();
+  var currentPriceCell = newRow.insertCell();
+  var timeCell = newRow.insertCell();
 
   var openText = document.createTextNode("$" + stonksData.o);
-  var hText = document.createTextNode("$" + stonksData.h);
-  var lText = document.createTextNode("$" + stonksData.l);
-  var cText = document.createTextNode("$" + stonksData.o);
-  var pText = document.createTextNode("$" + stonksData.h);
-  var tText = document.createTextNode(Date(stonksData.t));
+  var highText = document.createTextNode("$" + stonksData.h);
+  var lowText = document.createTextNode("$" + stonksData.l);
+  var closingText = document.createTextNode("$" + stonksData.o);
+  var priceText = document.createTextNode("$" + stonksData.h);
+  var timeText = document.createTextNode(date);
 
-  oCell.appendChild(openText);
-  hCell.appendChild(lText);
-  lCell.appendChild(hText);
-  cCell.appendChild(cText);
-  pCell.appendChild(pText);
-  tCell.appendChild(tText);
+  openCell.appendChild(openText);
+  highCell.appendChild(lowText);
+  lowCell.appendChild(highText);
+  closingCell.appendChild(closingText);
+  currentPriceCell.appendChild(priceText);
+  timeCell.appendChild(timeText);
 
   setTimeout(() => {
-    dispInfo(symbol, time); //dis works
+    dispInfo(symbol, time);
   }, time);
 }
